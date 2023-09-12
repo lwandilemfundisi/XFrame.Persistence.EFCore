8586a6e2ab781a39cc2f18ae1a364d9f2710ffa1
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-09-12 20:33:59 +0200

    Added value object converters

---
+++ XFrame.Persistence.EFCore/ValueObjectConverters/IdentityValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using XFrame.Ids;
+using XFrame.ValueObjects.SingleValueObjects;
+
+namespace XFrame.Persistence.EFCore.ValueObjectConverters
+{
+    public class IdentityValueConverter<TIdentity> : ValueConverter<TIdentity, string>
+        where TIdentity : Identity<TIdentity>
+    {
+        public IdentityValueConverter(ConverterMappingHints mappingHints = null)
+        : base(
+            id => id.Value,
+            value => typeof(TIdentity).GetConstructor(new Type[] { typeof(string) }).Invoke(new object[] { value }) as TIdentity,
+            mappingHints
+        )
+        { }
+    }
+
+    public class SingleValueObjectIdentityValueConverter<TIdentity> : ValueConverter<TIdentity, string>
+        where TIdentity : SingleValueObject<string>, IIdentity
+    {
+        public SingleValueObjectIdentityValueConverter(ConverterMappingHints mappingHints = null)
+        : base(
+            id => id.Value,
+            value => typeof(TIdentity).GetConstructor(new Type[] { typeof(string) }).Invoke(new object[] { value }) as TIdentity,
+            mappingHints
+        )
+        { }
+    }
+}
---
+++ XFrame.Persistence.EFCore/ValueObjectConverters/ValueObjectValueConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using XFrame.ValueObjects.XmlValueObjects;
+
+namespace XFrame.Persistence.EFCore.ValueObjectConverters
+{
+    public class ValueObjectValueConverter<TValueObjectType, TValueObjectTypes> : ValueConverter<TValueObjectType, string>
+        where TValueObjectType : XmlValueObject
+        where TValueObjectTypes : XmlValueObjectLookup<TValueObjectType, TValueObjectTypes>
+    {
+        public ValueObjectValueConverter(ConverterMappingHints mappingHints = null)
+        : base(
+            valueObject => valueObject.Code,
+            value => XmlValueObjectLookup<TValueObjectType, TValueObjectTypes>.Of().AllowedItems.FirstOrDefault(s => s.Code == value),
+            mappingHints
+        )
+        { }
+    }
+}
